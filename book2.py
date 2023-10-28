a547f357d85e6709165fc7491d9a13975816e0e8
s-md-ahmed <115739683+s-md-ahmed@users.noreply.github.com>
2023-10-28 22:37:11 +0530

    Update book2.py

--- book2.py
+++ book2.py
@@ -1,10 +1,12 @@
+# Import required modules and create a FastAPI app
 from typing import Optional
-
-from pydantic import BaseModel,Field
+from pydantic import BaseModel, Field
 from fastapi import FastAPI, Path, Query, HTTPException
-app = FastAPI()
 from starlette import status
 
+app = FastAPI()
+
+# Define a Book class to represent book data
 class Book:
     id: int
     title: str
@@ -21,9 +23,9 @@ class Book:
         self.rating = rating
         self.published_date = published_date
 
-
+# Define a Pydantic model for the request body
 class Bookrequest(BaseModel):
-    id: Optional[int] = Field(None,title="Id is Not needed")
+    id: Optional[int] = Field(None, title="Id is Not needed")
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
@@ -31,58 +33,64 @@ class Bookrequest(BaseModel):
     published_date: int
 
     class Config:
-        json_schema_extra = {'example':{'title': 'New book', 'author':'coding with Roby', 'description':'Fantastic', 'rating': 5, 'published_date': 2023}}
+        json_schema_extra = {'example': {
+            'title': 'New book',
+            'author': 'coding with Roby',
+            'description': 'Fantastic',
+            'rating': 5,
+            'published_date': 2023
+        }}
 
+# Sample book data
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'coding with roby', 'Very Good', 4.5, 2023),
+    Book(2, 'FastAPI', 'coding with roby', 'Good', 5, 2024),
+    Book(3, 'Data Science', 'coding with eric', 'Very Good', 5, 2025),
+    Book(4, 'Computer Science Pro', 'coding with roby', 'Very Good', 4.5, 2026)
+]
 
-BOOKS = [Book(1, 'Computer Science Pro', 'coding with roby', 'Very Good',4.5,2023),Book(2, 'FastAPI', 'coding with roby', 'Good',5,2024),Book(3, 'Data Science', 'coding with eric', 'Very Good',5,2025),Book(4, 'Computer Science Pro', 'coding with roby', 'Very Good',4.5,2026)]
-
-
+# Route to get all books
 @app.get("/books", status_code=status.HTTP_200_OK)
-def bookreturn():
+def book_return():
     return BOOKS
 
-
+# Route to get a book by its ID
 @app.get("/books/{bookid}", status_code=status.HTTP_200_OK)
-def getbookbyid(bookid:int = Path(gt=0)):
+def get_book_by_id(bookid: int = Path(gt=0)):
     for book in BOOKS:
         if bookid == book.id:
             return book
-    raise HTTPException(status_code=404 , detail="Item Not Found")
+    raise HTTPException(status_code=404, detail="Item Not Found")
 
-
+# Route to get books by rating
 @app.get("/book/", status_code=status.HTTP_200_OK)
-def getbookbyrating(book_rating:int=Query(gt=0,lt=6)):
-    bookstoappend = []
+def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_append = []
     for book in BOOKS:
         if book_rating == book.rating:
-            bookstoappend.append(book)
-    return bookstoappend
+            books_to_append.append(book)
+    return books_to_append
 
-
+# Route to get books by published date
 @app.get("/book/publishdate", status_code=status.HTTP_200_OK)
-def getbookbydate(publishdate:int = Query(gt=1000,lt=2031)):
-    bookstoappend=[]
+def get_book_by_date(publishdate: int = Query(gt=1000, lt=2031)):
+    books_to_append = []
     for book in BOOKS:
         if book.published_date == publishdate:
-            bookstoappend.append(book)
-    return bookstoappend
+            books_to_append.append(book)
+    return books_to_append
 
-
+# Route to create a new book
 @app.post("/books/create-book", status_code=status.HTTP_201_CREATED)
-def createbook(book_request:Bookrequest):
+def create_book(book_request: Bookrequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(findbookid(new_book))
+    BOOKS.append(find_book_id(new_book))
 
-
-def findbookid(book: BOOKS):
-    book.id = 1 if len(BOOKS)==0 else BOOKS[-1].id+1
-    # if len(BOOKS) > 0:
-    #     book.id = BOOKS[-1].id+1
-    # else:
-    #     book.id=1
+def find_book_id(book: BOOKS):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
 
-
+# Route to update a book
 @app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: Bookrequest):
     book_changed = False
@@ -93,15 +101,14 @@ async def update_book(book: Bookrequest):
     if not book_changed:
         raise HTTPException(status_code=404, detail='Item not found')
 
-
-
+# Route to delete a book by ID
 @app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-def deletebook(book_id:int = Path(gt=0)):
+def delete_book(book_id: int = Path(gt=0)):
     book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
-            book_changed=True
+            book_changed = True
             break
     if not book_changed:
-        raise HTTPException(status_code=404 , detail="Item Not Found")
+        raise HTTPException(status_code=404, detail="Item Not Found")
